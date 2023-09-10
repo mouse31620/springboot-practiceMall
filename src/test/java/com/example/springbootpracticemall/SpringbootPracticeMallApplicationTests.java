1029596adc296531671fe1c4a402e04b4a2b40e8
Justin Wu <mouse31620@gmail.com>
2023-09-10 20:26:01 +0800

    init spring boot project

---
+++ src/test/java/com/example/springbootpracticemall/SpringbootPracticeMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootpracticemall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPracticeMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
