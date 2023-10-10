9263ba5f389a072ed80ea2b99ed96462f8830d17
Justin Wu <mouse31620@gmail.com>
2023-10-10 11:14:58 +0800

    商品功能 - Refactor category enum

---
+++ src/main/java/com/example/springbootpracticemall/constant/ProductCategory.java
@@ -0,0 +1,7 @@
+package com.example.springbootpracticemall.constant;
+
+public enum ProductCategory {
+    FOOD,
+    CAR,
+    BOOK
+}
--- src/main/java/com/example/springbootpracticemall/rowmapper/ProductRowMapper.java
+++ src/main/java/com/example/springbootpracticemall/rowmapper/ProductRowMapper.java
@@ -1,5 +1,6 @@
 package com.example.springbootpracticemall.rowmapper;
 
+import com.example.springbootpracticemall.constant.ProductCategory;
 import com.example.springbootpracticemall.model.Product;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -12,7 +13,9 @@ public class ProductRowMapper implements RowMapper<Product> {
         Product product = new Product();
         product.setProductId(resultSet.getInt("product_id"));
         product.setProductName(resultSet.getString("product_name"));
-        product.setCategory(resultSet.getString("category"));
+        String categoryStr = resultSet.getString("category");
+        ProductCategory category = ProductCategory.valueOf(categoryStr);
+        product.setCategory(category);
         product.setImageUrl(resultSet.getString("image_url"));
         product.setPrice(resultSet.getInt("price"));
         product.setStock(resultSet.getInt("stock"));
