001f2baaf3e4766b75cf1e19c1c2a3973a6d592f
Justin Wu <mouse31620@gmail.com>
2024-10-21 20:38:47 +0800

    增加security管理條件

--- src/main/java/com/example/springbootpracticemall/security/SecurityConfig.java
+++ src/main/java/com/example/springbootpracticemall/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.springbootpracticemall.security;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -29,7 +30,23 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, JwtAuthenticationFilter jwtAuthFilter) throws Exception {
         return httpSecurity
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(requests -> requests.anyRequest().permitAll())
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/", "/index.html", "/login.html", "/register.html", "/css/**", "/js/**", "/images/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/products/**", "/orders/**").permitAll()
+                        .requestMatchers("/users/login", "/users/register").permitAll()
+                        //產品管理權限
+                        .requestMatchers(HttpMethod.POST, "/products/**").hasAuthority("PRODUCT_MANAGE")
+                        .requestMatchers(HttpMethod.PUT, "/products/**").hasAuthority("PRODUCT_MANAGE")
+                        .requestMatchers(HttpMethod.DELETE, "/products/**").hasAuthority("PRODUCT_MANAGE")
+
+                        // 訂單管理權限
+                        .requestMatchers(HttpMethod.POST, "/orders/**").hasAuthority("ORDER_MANAGE")
+                        .requestMatchers(HttpMethod.PUT, "/orders/**").hasAuthority("ORDER_MANAGE")
+
+                        // 用戶管理權限
+                        .requestMatchers(HttpMethod.POST, "/users/**").hasAuthority("USER_MANAGE")
+                        .requestMatchers(HttpMethod.PUT, "/users/**").hasAuthority("USER_MANAGE")
+                        .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthFilter, BasicAuthenticationFilter.class)
                 .build();
     }
